74932af6da1fa7b1f0779b34ef6b1b46fd0fe6fc
anatsheh <nat.ahmad@gmail.com>
2021-10-27 10:53:53 +0300

    new line

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,5 +1,4 @@
 <?php
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
-  print "This is Dawiat Demo" . "\n";
-  echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
+  print "\n This is Dawiat Demo" . "\n";
 ?>
